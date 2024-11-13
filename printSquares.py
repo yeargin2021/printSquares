f90d8f190fdb89292034ac91ba0876610b974c6c
Tommy H. Yeargin, Jr <88904457+yeargin2021@users.noreply.github.com>
2024-11-13 04:58:33 -0500

    First commit

---
+++ printSquares.py
@@ -0,0 +1,9 @@
+# Prints the squares of numbers from 1 to 5
+# in the format: number - number squared.
+#
+# 13 November 2025
+
+myrange = range(5)
+
+for i in myrange:
+    print(f"{i+1} - {(i+1)**2}")
